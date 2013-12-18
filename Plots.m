0c4363468b60225616ba765ce38ef3017a61c404
Schellenberger <schellenberger@inb.uni-luebeck.de>
2013-12-18 11:42:58 +0100

    Added a link to Data to plot N2 EEG data. 	Compare Model and EEG of N2 and N3 simulataneously
    	modified:   Plots.m
    	modified:   parameters.h

--- Plots.m
+++ Plots.m
@@ -3,53 +3,49 @@
 
 function Plots(T, onset, Input)
 
-addpath('~/Documents/MATLAB/TC_model/Data');        % Data folder
-
-% load the data the model should be fit to
-load('S13_2');
-Data=zscore(double(data30(6,(391*100):(630*100))))';
-
-
 if nargin == 0
     % fittet input
-    Input_fit            = [102.8;		% N_ee
-                            72.3;			% N_ei
-                            100.5;		% N_ie
-                            100.5; 		% N_ii
-                            3.6322;        % alpha_Na
-                            2.718;		% tau_Na
-                            0.6347;			% g_KNa
-                            24.36;		% tau_e
-                            29.75; 		% tau_i
-                            -64.5536;        % theta_e
-                            8.3553];      % sigma_e
+    Input_N3    = [ 102;         % N_ee
+                    72;			% N_ei
+                    100;		% N_ie
+                    100; 		% N_ii
+                    3.6322;     % alpha_Na
+                    2.7;		% tau_Na
+                    0.6347;		% g_KNa
+                    25;         % tau_e
+                    30; 		% tau_i
+                    -64.5536;   % theta_e
+                    8.3553];	% sigma_e
                         
-    Input_orig            = [120;		% N_ee
-                            70;			% N_ei
-                            90;		% N_ie
-                            90; 		% N_ii
-                            2;        % alpha_Na
-                            1;		% tau_Na
-                            1.33;			% g_KNa
-                            30;		% tau_e
-                            30; 		% tau_i
-                            -58.5;        % theta_e
-                            4];      % sigma_e
-    T          = 30;			% duration of the simulation
-    onset    = 10;			% time until data is saved
-    
-    var_stim   = [1E-3;			% strength of the stimulus
-                  1050E1;		% time until stimulus         in 0.1 ms
-                  50E1;			% duration of the stimulus in 0.1 ms
-                  1;			% number of stimuli
-                  10];			% minimal time between stimulations
+    Input_N2    = [ 120;	% N_ee
+                    60;			% N_ei
+                    90;         % N_ie
+                    90;         % N_ii
+                    2.;         % alpha_Na
+                    1;          % tau_Na
+                    1.33;		% g_KNa
+                    30;         % tau_e
+                    30; 		% tau_i
+                    -58.5;      % theta_e
+                    4];         % sigma_e
+                        
+    var_stim    = [ 0;          % strength of the stimulus in Hz (spikes per second)
+                    0;          % time between   stimuli in s    
+                    0;          % time until first stimuli in s
+                    0];        % duration of the stimulus in ms
+    T           =   30;			% duration of the simulation
+    onset       =   10;			% time until data is saved
 end
 
-[Ve, ~]    = Cortex(T, onset, Input_orig);
+[Ve_N2]    = Cortex(T, onset, Input_N2, var_stim);
+[Ve_N3]    = Cortex(T, onset, Input_N3, var_stim);
 
-L           = max(size(Ve));
-fs          = L/T;
-timeaxis    = linspace(0,T,L);
+% load the data the model should be fit to
+addpath('~/Documents/MATLAB/TC_model/Data');        % Data folder
+load('N2data');
+Data_N2=zscore(data)';
+load('S13_2', 'data30');
+Data_N3=zscore(double(data30(6,(391*100):(630*100))))';
 
 ImageFontSize=16;
 TitleFontSize=20;
@@ -58,16 +54,22 @@ AxisFontName='CMU Serif';
 set(gca,'FontName',AxisFontName,'FontSize',ImageFontSize)
 % plottig the result with an random T second snipplet from the Data
 time = linspace(0,T,T*100);
-i    = randi([0,239-T],1);
+i_N2    = randi([0,150-T],1);
+i_N3    = randi([0,239-T],1);
 
 figure(1)
-subplot(211)
-plot(time,zscore(Ve'))
-title('pyramidal membrane voltage','FontSize',TitleFontSize),  xlabel('time in s','FontSize',ImageFontSize), ylabel('V_{e} in \muV','FontSize',ImageFontSize)
-subplot(212)
-plot(time,Data(i*100+1:(i+T)*100))
-title('EEG trace','FontSize',TitleFontSize),  xlabel('time in s','FontSize',ImageFontSize), ylabel('V_{e} in \muV','FontSize',ImageFontSize)
-
+subplot(221)
+plot(time,zscore(Ve_N2)')
+title('Model in N2','FontSize',TitleFontSize),  xlabel('time in s','FontSize',ImageFontSize), ylabel('V_{e} in \muV','FontSize',ImageFontSize)
+subplot(222)
+plot(time,zscore(Ve_N3)')
+title('Model in N3','FontSize',TitleFontSize),  xlabel('time in s','FontSize',ImageFontSize), ylabel('V_{e} in \muV','FontSize',ImageFontSize)
+subplot(223)
+plot(time,Data_N2(i_N2*100+1:(i_N2+T)*100))
+title('EEG trace of N2','FontSize',TitleFontSize),  xlabel('time in s','FontSize',ImageFontSize), ylabel('V_{e} in \muV','FontSize',ImageFontSize)
+subplot(224)
+plot(time,Data_N3(i_N3*100+1:(i_N3+T)*100))
+title('EEG trace of N3','FontSize',TitleFontSize),  xlabel('time in s','FontSize',ImageFontSize), ylabel('V_{e} in \muV','FontSize',ImageFontSize)
 %exportfig(gcf, 'C_K_complex.png', 'Format', 'png', 'width', 12.8, 'Color', 'rgb')
 %  
 % [Pxx,f] = pwelch(Ve-mean(Ve),[], [],[], fs);
