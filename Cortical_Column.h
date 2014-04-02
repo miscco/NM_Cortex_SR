b4563e7309755ab79a763598e812a524032efd4b
Schellenberger <schellenberger@inb.uni-luebeck.de>
2014-04-02 14:12:15 +0200

    Initialized variable vectors within class definition not in constructor.
    Signed-off-by: Schellenberger <schellenberger@inb.uni-luebeck.de>

--- Cortical_Column.h
+++ Cortical_Column.h
@@ -28,16 +28,10 @@ class Cortical_Column {
 public:
 	// Constructors
 	Cortical_Column(void)
-	: Ve		(_INIT(E_L_e)),	Vi 	   		(_INIT(E_L_i)),	Na	 		(_INIT(Na_eq)),
-	  Phi_ee	(_INIT(0.0)), 	Phi_ei 		(_INIT(0.0)), 	Phi_ie 		(_INIT(0.0)), 	Phi_ii	(_INIT(0.0)),
-	  x_ee 		(_INIT(0.0)), 	x_ei   		(_INIT(0.0)),	x_ie   		(_INIT(0.0)), 	x_ii	(_INIT(0.0))
 	{set_RNG();}
 
 	Cortical_Column(double* Par)
-	: Ve		(_INIT(E_L_e)),	Vi 	   		(_INIT(E_L_i)),	Na	 		(_INIT(Na_eq)),
-	  Phi_ee	(_INIT(0.0)), 	Phi_ei 		(_INIT(0.0)), 	Phi_ie 		(_INIT(0.0)), 	Phi_ii		(_INIT(0.0)),
-	  x_ee 		(_INIT(0.0)), 	x_ei   		(_INIT(0.0)),	x_ie   		(_INIT(0.0)), 	x_ii		(_INIT(0.0)),
-	  tau_e		(Par[0]), 		theta_e		(Par[1]),		sigma_e 	(Par[2]),		alpha_Na 	(Par[3]),
+	 :tau_e		(Par[0]), 		theta_e		(Par[1]),		sigma_e 	(Par[2]),		alpha_Na 	(Par[3]),
 	  tau_Na	(Par[4]),	  	g_KNa		(Par[5]),		dphi_c		(Par[6])
 	{set_RNG();}
 
@@ -75,17 +69,17 @@ public:
 
 private:
 	// Population variables
-	vector<double> 	Ve,			// excitatory membrane voltage
-					Vi,			// inhibitory membrane voltage
-					Na,			// Na concentration
-					Phi_ee,		// PostSP from excitatory to excitatory population
-					Phi_ei,		// PostSP from excitatory to inhibitory population
-					Phi_ie,		// PostSP from inhibitory to excitatory population
-					Phi_ii,		// PostSP from inhibitory to inhibitory population
-					x_ee,		// derivative of Phi_ee
-					x_ei,		// derivative of Phi_ei
-					x_ie,		// derivative of Phi_ie
-					x_ii;		// derivative of Phi_ii
+	vector<double> 	Ve		= _INIT(E_L_e),		// excitatory membrane voltage
+					Vi		= _INIT(E_L_i),		// inhibitory membrane voltage
+					Na		= _INIT(Na_eq),		// Na concentration
+					Phi_ee	= _INIT(0.0),		// PostSP from excitatory to excitatory population
+					Phi_ei	= _INIT(0.0),		// PostSP from excitatory to inhibitory population
+					Phi_ie	= _INIT(0.0),		// PostSP from inhibitory to excitatory population
+					Phi_ii	= _INIT(0.0),		// PostSP from inhibitory to inhibitory population
+					x_ee	= _INIT(0.0),		// derivative of Phi_ee
+					x_ei	= _INIT(0.0),		// derivative of Phi_ei
+					x_ie	= _INIT(0.0),		// derivative of Phi_ie
+					x_ii	= _INIT(0.0);		// derivative of Phi_ii
 
 	// Random number generators
 	vector<GEN>		MTRands;
